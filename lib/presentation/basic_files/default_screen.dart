72565c6d22fde4b2958d38840fc2232026487f50
shimaa <shimaa.shams@gmail.com>
2024-09-20 18:54:00 +0300

    feat:create 4 tabs and bottom navigation bar

---
+++ lib/presentation/basic_files/default_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app_2024/presentation/categories_screen/categories_sreen.dart';
+import 'package:movies_app_2024/presentation/home/home_screen.dart';
+import 'package:movies_app_2024/presentation/search_screen/search_screen.dart';
+import 'package:movies_app_2024/presentation/watch_list_screen/watch_list_screen.dart';
+import 'BottomNavItem.dart';
+
+class DefaultScreen extends StatefulWidget {
+  const DefaultScreen({super.key});
+
+  static const String routeName = "DefaultScreen";
+
+  @override
+  State<DefaultScreen> createState() => _DefaultScreenState();
+}
+
+class _DefaultScreenState extends State<DefaultScreen> {
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: tabs[selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        currentIndex: selectedIndex,
+        items: [
+          BottomNavItem("HOME", 'iconHome.png'),
+          BottomNavItem("SEARCH", 'iconSearch.png'),
+          BottomNavItem("BROWSE", 'iconCategories.png'),
+          BottomNavItem("WATCHLIST", 'iconWatchList.png'),
+        ],
+      ),
+
+    );
+  }
+  var tabs = [HomeScreen(), SearchScreen(),CategoriesScreen(), WatchListScreen()];
+}
---
+++ lib/presentation/categories_screen/categories_sreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  static const String routeName = "CategoriesScreen";
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
