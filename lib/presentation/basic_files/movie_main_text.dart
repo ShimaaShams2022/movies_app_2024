8958903854b1f323df0ea68b19535095a0161b81
shimaa <shimaa.shams@gmail.com>
2024-09-24 21:01:53 +0300

    feat:add API,bloc,home view model error:don't show popular Movies

--- lib/presentation/basic_files/movie_main_text.dart
+++ lib/presentation/basic_files/movie_main_text.dart
@@ -1,34 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app_2024/data/api_model/Results.dart';
 
 import 'my_theme/my_theme_data.dart';
 
 class MovieMainText extends StatelessWidget {
-  const MovieMainText({super.key});
+   MovieMainText({required this.adsMovie,super.key});
+
+  Results adsMovie;
+
+
+
 
   @override
   Widget build(BuildContext context) {
+
+    bool isAdult=adsMovie.adult ?? false;
     return  Column(
       crossAxisAlignment:CrossAxisAlignment.start,
 
       children: [
         Text(
-          "Dora and the lost city of gold",
+          adsMovie.originalTitle??"",
           style: MyThemeData.darkTheme.textTheme.bodyLarge,
         ),
         SizedBox(height: 10),
         Row(
           children: [
-            Text(
-              "2019",
-              style: MyThemeData.darkTheme.textTheme.bodyMedium,
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Text(
+                adsMovie.releaseDate??"",
+                style: MyThemeData.darkTheme.textTheme.bodyMedium,
+              ),
             ),
-            Text(
-              "PG-13",
-              style: MyThemeData.darkTheme.textTheme.bodyMedium,
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Text(isAdult ? "+18":"PG-13" ,
+                style: MyThemeData.darkTheme.textTheme.bodyMedium,
+              ),
             ),
-            Text(
-              "2h 7m",
-              style: MyThemeData.darkTheme.textTheme.bodyMedium,
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Text(
+                adsMovie.originalLanguage??"",
+                style: MyThemeData.darkTheme.textTheme.bodyMedium,
+              ),
             )
           ],
         ),
